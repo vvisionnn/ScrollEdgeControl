9a22cb2a35d8bc250a42f7663f44d2418cc5295d
vvisionnn <0xEFEFEF@gmail.com>
2023-03-25 01:47:09 +0800

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -9,7 +9,7 @@ let package = Package(
     .library(name: "ScrollEdgeControlComponents", type: .static, targets: ["ScrollEdgeControlComponents"]),
   ],
   dependencies: [
-    .package(url: "http://github.com/timdonnelly/Advance", .branch("master"))
+    .package(url: "https://github.com/timdonnelly/Advance", .branch("master"))
   ],
   targets: [
     .target(
